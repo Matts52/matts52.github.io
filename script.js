d09320999a8855d4434ebb1e73bb0bd77a2030e3
Matt Senick <32623708+Matts52@users.noreply.github.com>
2023-03-27 00:02:35 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,62 @@
+// Get all sections on the page
+const allSections = document.querySelectorAll('section');
+
+// Add a scroll event listener
+window.addEventListener('scroll', () => {
+  // Loop through each section
+  allSections.forEach(section => {
+    // Check if the section is in the viewport
+    const sectionTop = section.getBoundingClientRect().top;
+    const sectionBottom = section.getBoundingClientRect().bottom;
+    const viewportHeight = window.innerHeight;
+    if (sectionTop < viewportHeight && sectionBottom > 0) {
+      // If the section is in view, add the 'in-view' class
+      section.classList.add('in-view');
+    } else {
+      // If the section is not in view, remove the 'in-view' class
+      section.classList.remove('in-view');
+    }
+  });
+});
+
+
+
+// Random shape generation
+const svg = document.querySelector('.background-shapes');
+
+// Randomize the position of each shape
+const shapes = svg.querySelectorAll('circle, rect');
+shapes.forEach(shape => {
+  const x = Math.floor(Math.random() * 80) + 10; // Random number between 10 and 90
+  const y = Math.floor(Math.random() * 80) + 10; // Random number between 10 and 90
+  const op = Math.random() * 0.9; // Random number between 10 and 90
+  const r = Math.floor(Math.random() * 255); 
+  const g = Math.floor(Math.random() * 255); 
+  const b = Math.floor(Math.random() * 255); 
+
+  shape.setAttribute('cx', x);
+  shape.setAttribute('cy', y);
+  shape.setAttribute('x', x);
+  shape.setAttribute('y', y);
+  shape.setAttribute('opacity', op);
+  shape.setAttribute('fill', "rgb("+r.toString()+","+g.toString()+","+b.toString()+")")
+});
+
+
+
+
+
+
+// Dark Mode toggling
+/*
+const darkModeToggle = document.querySelector("#dark-mode-toggle");
+
+darkModeToggle.addEventListener("click", function() {
+    document.body.classList.toggle("dark-mode");
+  });
+*/
+
+
+
+
+
